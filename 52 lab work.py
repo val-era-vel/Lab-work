e77ebdd1b9414c2bcf288a3bd08cf5a12bce9614
val-era-vel <valeria.velichko.2007@gmail.com>
2025-09-19 00:44:28 +0300

    Add files via upload

---
+++ 52 lab work.py
@@ -0,0 +1,16 @@
+a = float (input("masa1:"))
+b = float (input("masa2:"))
+d = float (input("masa3:"))
+a1 = a*1000
+b1 = b *1000
+d1 = d*1000
+tisto = (a1*40)/100
+flour = (tisto * 70)/100
+tisto1 = (b1*40)/100
+flour1 = (tisto1 * 70)/100
+tisto2 = (d1*40)/100
+flour2 = (tisto2 * 70)/100
+print(flour)
+print(flour1)
+print(flour2)
+
