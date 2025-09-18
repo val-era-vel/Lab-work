e03e7d26c646e4f6559300f0b0efc6c1bcf7b179
val-era-vel <valeria.velichko.2007@gmail.com>
2025-09-19 00:48:10 +0300

    Add files via upload

---
+++ 80 lab work.py
@@ -0,0 +1,10 @@
+num = input("give me a number: ")
+a = int(num[0])
+b = int(num[1])
+c = int(num[2])
+d = int(num[3])
+e = int(num[4])
+k = a + c + e
+g = b + d
+result = str(k) + str(g)
+print("Відповідь:", result)
