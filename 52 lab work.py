30e1572b30ddea927300038aac028fad748bb0e4
val-era-vel <valeria.velichko.2007@gmail.com>
2025-09-19 00:47:02 +0300

    Update 52 lab work.py

--- 52 lab work.py
+++ 52 lab work.py
@@ -1,16 +1,6 @@
-a = float (input("masa1:"))
-b = float (input("masa2:"))
-d = float (input("masa3:"))
-a1 = a*1000
-b1 = b *1000
-d1 = d*1000
-tisto = (a1*40)/100
-flour = (tisto * 70)/100
-tisto1 = (b1*40)/100
-flour1 = (tisto1 * 70)/100
-tisto2 = (d1*40)/100
-flour2 = (tisto2 * 70)/100
-print(flour)
-print(flour1)
-print(flour2)
+a = float(input("how much:"))
+b = float(input("percent:"))
+x = (a * b) / 100
+h = a + x
+print("sum of money", h)
 
