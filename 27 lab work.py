b588a22a2418171131313459d22b1d619f74ffda
val-era-vel <valeria.velichko.2007@gmail.com>
2025-09-19 00:42:14 +0300

    Add files via upload

---
+++ 27 lab work.py
@@ -0,0 +1,10 @@
+f = float (input("Enter the desired future value: "))
+r = float (input("Enter the annual interest rate:"))
+n = float(input("Enter the number of years the money will grow:"))
+p = f/(1+r)**n
+print("You will need to deposit this amount", p)
+
+
+
+
+
