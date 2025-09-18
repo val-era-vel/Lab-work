c08983fb8d76b1005e1615dead26113a89c23c3c
val-era-vel <valeria.velichko.2007@gmail.com>
2025-09-19 00:40:25 +0300

    Add files via upload

---
+++ 2 variant 1 lab work in vntu.py
@@ -0,0 +1,11 @@
+a = int(input("give a number:"))
+b = int(input("give a number:"))
+k = int(input("give a number:"))
+c = a+ a*11 + a*111
+n = b+ b*11 + b*111
+m = k+ k*11 + k*111
+print(c)
+print(n)
+print(m)
+
+
